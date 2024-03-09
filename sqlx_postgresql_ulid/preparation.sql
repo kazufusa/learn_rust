6efeeeb79a8ea0d2324f988f7db9aca4dcc3987b
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2024-03-09 22:00:04 +0900

    fix: 🐛 SQLx and ULID: introduce ulid generator in SQL

--- sqlx_postgresql_ulid/preparation.sql
+++ sqlx_postgresql_ulid/preparation.sql
@@ -1,9 +1,15 @@
 BEGIN;
 
+CREATE DOMAIN ulid AS VARCHAR(26);
+
 CREATE TABLE users (
-    id CHAR(26) PRIMARY KEY,
-    name TEXT NOT NULL,
+    id         ulid PRIMARY KEY DEFAULT generate_ulid(),
+    name       TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO users (name) VALUES ('alpha'), ('beta');
+
+SELECT * FROM users;
 
 COMMIT;
